54b9a7d2747fecc0e07b56a1038aebb0e4b12dc1
Eli Keetch <elikeetch1996@gmail.com>
2017-06-19 13:52:31 -0600

    updated mini proj, 06-19-17

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
 
+
 import Nav from './Nav/Nav';
 import Cart from './Cart/Cart';
+
+import router from '../router.js';
 
 export default function App() {
   return (
@@ -10,6 +13,7 @@ export default function App() {
       <Nav />
       <div id="App__mainContainer">
         <div id="App__routerContainer">
+          { router }
           <Cart />
         </div>
       </div>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 import './Nav.css';
+import {Link} from 'react-router-dom';
 
 import ShoppingCart from 'react-icons/lib/fa/shopping-cart';
 
@@ -9,12 +10,16 @@ function Nav( { items } ) {
   return (
     <div id="Nav__container">
       <div id="Nav__linksContainer">
+        <Link className="navLink" to="/" >
         <span className="Nav__label">Store</span>
+        </Link>
 
+        <Link className="navLink" to="/checkout">
         <div id="Nav__checkoutContainer">
           <div id="Nav__itemsInCart"> { items } </div>
           <ShoppingCart id="Nav__shoppingCart" />
         </div>
+        </Link>
       </div>
     </div>
   )
